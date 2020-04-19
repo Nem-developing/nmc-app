0fd42e7bcb39eaf37d6e77f4619976ed1b095550
Nem <54665744+Nem-developing@users.noreply.github.com>
2020-04-19 17:27:17 +0200

    Ajout écoute bouton.

--- NemixApp/src/main/java/com/nemixcraft/nemixcraft/Error404.java
+++ NemixApp/src/main/java/com/nemixcraft/nemixcraft/Error404.java
@@ -1,38 +1,28 @@
 package com.nemixcraft.nemixcraft;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
-import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Error404 extends AppCompatActivity {
-
-    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_error404);
 
-        button = (Button) findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
+        configureNextButton();
+}
+    private void configureNextButton() {
+        Button nextButton = (Button) findViewById(R.id.button_switch);
+        nextButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                setContentView(R.layout.activity_home);
-                openmainactivity();
+                startActivity(new Intent(Error404.this, MainActivity.class));
             }
         });
-    }
-
-
-
-
-    public void openmainactivity() {
-        Toast.makeText(getApplicationContext(), "TEST", Toast.LENGTH_SHORT).show();
-        Intent intent = new Intent(this, HomeActivity.class);
-        startActivity(intent);
     }
 }
